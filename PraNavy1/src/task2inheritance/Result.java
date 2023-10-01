6c7879855c60bf63372a7087ae27675bdf813785
Navyasri12 <navyasri1209@gmail.com>
2023-10-01 17:34:02 +0530

    task2- inheritance 30.09.23

---
+++ PraNavy1/src/task2inheritance/Result.java
@@ -0,0 +1,12 @@
+package task2inheritance;
+
+public class Result {
+
+    public static void main(String[] args) {
+    	Car c = new Car("Porsche","Porsche 720",2000,2);
+    	c.display_info();
+    	Motorcycle m=new Motorcycle("pulsar","NS-200",2011,200);
+    	m.display_info();
+
+    }
+}
